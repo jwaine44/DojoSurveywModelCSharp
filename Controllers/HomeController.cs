6cbd26ff3d710035e5e046c958963b0f9827226d
jwaine44 <jwaine44@gmail.com>
2022-08-03 15:59:12 -0400

    Initialize Repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveywModel.Models;
+
+namespace DojoSurveywModel.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("result")]
+    public IActionResult Result(Survey result)
+    {
+        return View(result);
+    }
+
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace DojoSurveywModel.Models;
+
+public class Survey
+{
+    public string Name {get;set;}
+    public string Location {get;set;}
+    public string FavLanguage {get;set;}
+    public string? Comment {get;set;}
+}
